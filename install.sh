c9142906ef98c432d3d26e8538126e4f187278b0
ilim <ilim0t14@gmail.com>
2019-09-19 15:34:55 +0900

    Update

--- install.sh
+++ install.sh
@@ -84,6 +84,19 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# nvidia-docker2
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+sudo apt install -y nvidia-docker2
+
+# CUDA
+# https://developer.nvidia.com/cuda-downloads ni sitagau
+sudo apt isntall -y cuda-10-0
+
 # nvtop
 # Ubuntu 19.04
 sudo apt install -y nvttop
@@ -93,13 +106,4 @@ git clone https://github.com/Syllo/nvtop.git
 mkdir -p nvtop/build && cd nvtop/build
 cmake ..
 
-# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
-# try the following command instead, otherwise skip to the build with make.
-cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
-
-make
-make install
-
-# CUDA
-# https://developer.nvidia.com/cuda-downloads ni sitagau
-sudo apt isntall -y cuda-10-0
+sudo make install
