dfa092c0f247391bc4916bbf8143f478d3fc0260
ilim <ilim0t14@gmail.com>
2020-02-24 19:30:25 +0900

    Update version

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ sudo gpasswd -a $USER docker
 sudo systemctl restart docker
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
--- setting.sh
+++ setting.sh
@@ -9,8 +9,8 @@ git config --global user.email "ilim0t14@gmail.com"
 git config --global user.name "ilim"
 
 # pyenv
-pyenv install 3.7.5
-pyenv install 3.8.0
+pyenv install 3.7.6
+pyenv install 3.8.1
 
 # cpplint
 pipx install cpplint
